6ccb43cd1a1f12919403b0de8b880af613c9f3fd
zhangsan <zhangsan@qq.com>
2020-09-01 18:56:16 +0800

    测试冲突

--- src/main/java/com/atguigu/java/GitTest.java
+++ src/main/java/com/atguigu/java/GitTest.java
@@ -3,7 +3,7 @@ package com.atguigu.java;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello");//修改
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
