a5cf60e643f2e970d03bd7310bead31e09f156e4
zhangsan <zhangsan@qq.com>
2020-09-01 19:39:23 +0800

    right

--- src/main/java/com/atguigu/java/GitTest.java
+++ src/main/java/com/atguigu/java/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
         System.out.println("hello");//edit by hotfix second
         System.out.println("hello");//修改2
         System.out.println("hello");//test push
-        System.out.println("hello");
+        System.out.println("hello");//edit by right
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
