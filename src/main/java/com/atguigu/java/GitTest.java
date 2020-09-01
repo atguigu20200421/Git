0032047d934f6d6aa23f70e06c5d7ac5aea1c80a
zhangsan <zhangsan@qq.com>
2020-09-01 18:46:32 +0800

    第一次使用idea进行操作git

---
+++ src/main/java/com/atguigu/java/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.java;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
