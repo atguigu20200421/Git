2f8b8680d183036ab650938f4d0859fab621ecc0
zhangsan <zhangsan@qq.com>
2020-09-01 18:56:48 +0800

    测试冲突 hotfix

--- src/main/java/com/atguigu/java/GitTest.java
+++ src/main/java/com/atguigu/java/GitTest.java
@@ -3,7 +3,7 @@ package com.atguigu.java;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello");//edit by hotfix
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
